cc2dd0c0dda6594b08e29ee5b1462f835fc3805c
Kanata <thunderpooh3@gmail.com>
2020-06-16 22:20:20 +0900

    build environment dev

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCk7x_YJdVRZI_OQ7JMCh1Xz0l-ZeLvPkE",
+  authDomain: "evernote-clone-6862f.firebaseapp.com",
+  databaseURL: "https://evernote-clone-6862f.firebaseio.com",
+  projectId: "evernote-clone-6862f",
+  storageBucket: "evernote-clone-6862f.appspot.com",
+  messagingSenderId: "192752030464",
+  appId: "1:192752030464:web:e9639e9828f817c5c5d9e5"
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +24,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
